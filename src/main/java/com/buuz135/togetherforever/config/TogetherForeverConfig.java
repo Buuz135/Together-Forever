2c4373c2f82eaae51bef803dfa561e8c4eb7a4d9
Buuz135 <buuz135@hotmail.com>
2018-01-23 13:57:16 +0100

    Added config change event

--- src/main/java/com/buuz135/togetherforever/config/TogetherForeverConfig.java
+++ src/main/java/com/buuz135/togetherforever/config/TogetherForeverConfig.java
@@ -2,6 +2,10 @@ package com.buuz135.togetherforever.config;
 
 import com.buuz135.togetherforever.TogetherForever;
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = TogetherForever.MOD_ID)
 public class TogetherForeverConfig {
@@ -11,4 +15,14 @@ public class TogetherForeverConfig {
 
     @Config.Comment("If enabled, players in the same team will sync the gamestages")
     public static boolean gamestagesSync = true;
+
+    @Mod.EventBusSubscriber(modid = TogetherForever.MOD_ID)
+    private static class EventHandler {
+        @SubscribeEvent
+        public static void onConfigChanged(final ConfigChangedEvent.OnConfigChangedEvent event) {
+            if (event.getModID().equals(TogetherForever.MOD_ID)) {
+                ConfigManager.sync(TogetherForever.MOD_ID, Config.Type.INSTANCE);
+            }
+        }
+    }
 }
