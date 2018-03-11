7f7ea8400ee32e0849b9cdf4835c5be57452e439
Buuz135 <buuz135@hotmail.com>
2018-03-11 21:33:10 +0100

    Added TC build

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 1.0.2
+
+
 # 1.0.1
 + Safe check for missing players when accepting invites
 + Actions now get synced when a player joins a team
--- src/main/java/com/buuz135/togetherforever/TogetherForever.java
+++ src/main/java/com/buuz135/togetherforever/TogetherForever.java
@@ -37,7 +37,7 @@ public class TogetherForever {
 
     public static final String MOD_ID = "togetherforever";
     public static final String MOD_NAME = "TogetherForever";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     public static Logger LOGGER;
 
